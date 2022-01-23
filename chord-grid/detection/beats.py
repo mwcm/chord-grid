d80a41687c7ff941a11a435c2c7a6a7dc6d6a0f1
morgan <mwcmitchell@gmail.com>
2022-01-22 18:06:13 -0800

    progress on downbeats

--- chord-grid/detection/beats.py
+++ chord-grid/detection/beats.py
@@ -1,28 +1,40 @@
 from madmom.features.tempo import TempoEstimationProcessor
-from madmom.features.beats import (RNNBeatProcessor, MultiModelSelectionProcessor,
-    BeatDetectionProcessor, CRFBeatDetectionProcessor, DBNBeatTrackingProcessor,
-    BeatTrackingProcessor)
+from madmom.features.beats import (
+    RNNBeatProcessor, MultiModelSelectionProcessor, BeatDetectionProcessor,
+    CRFBeatDetectionProcessor, DBNBeatTrackingProcessor, BeatTrackingProcessor
+)
 
 
 class Beats:
 
-    def __init__(self, fps=100, min_bpm=15, max_bpm=215, nn_files=None, tempo_estimator=None):
+    def __init__(self, fps=100, min_bpm=15.0, max_bpm=215.0, nn_files=None, tempo_estimator=None):
         self.fps = fps
         self.min_bpm = min_bpm
         self.max_bpm = max_bpm
         self.nn_files = nn_files
-        self.tempo_estimator = tempo_estimator or TempoEstimationProcessor(min_bpm=self.min_bpm, max_bpm=self.max_bpm)
+        self.tempo_estimator = TempoEstimationProcessor(min_bpm=self.min_bpm, max_bpm=self.max_bpm)
+        self.tempo_estimator = tempo_estimator or self.tempo_estimator
+
         # use post_processor = None to get all nn predictions
-        self.beat_predictor = RNNBeatProcessor(fps=self.fps, nn_files=self.nn_files, post_processor=None)
+        self.beat_predictor = RNNBeatProcessor(
+            fps=self.fps, nn_files=self.nn_files, post_processor=None
+        )
+
         # with None selected the MultiModelProcessor will averages all predictions
         # to obtain a reference prediction
         self.multi_model_prediction_processor = MultiModelSelectionProcessor(num_ref_predictions=None)
+
         self.beat_tracking_post_processor = BeatTrackingProcessor(fps=self.fps, tempo_estimator=tempo_estimator)
         self.beat_detection_post_processor = BeatDetectionProcessor(fps=self.fps)
+
         # NOTE: try to edit observation lambda and transition lambda here
-        self.dbn_post_processor = DBNBeatTrackingProcessor(fps=self.fps, min_bpm=self.min_bpm, max_bpm=self.max_bpm)
+        self.dbn_post_processor = DBNBeatTrackingProcessor(
+            fps=self.fps, min_bpm=self.min_bpm, max_bpm=self.max_bpm
+        )
+
         # NOTE: try some of the params on this one too?
         self.crf_post_processor = CRFBeatDetectionProcessor(fps=self.fps)
+
         self.post_processors = [
             self.beat_tracking_post_processor,
             self.beat_detection_post_processor,
@@ -31,13 +43,16 @@ class Beats:
         ]
         return
 
+
     def predict_beats(self, audio_file_path):
         predictions = self.beat_predictor(audio_file_path)
         return predictions
 
+
     def select_best_prediction(self, predictions):
         most_suitable_prediction = self.multi_model_prediction_processor(predictions)
         return most_suitable_prediction
+
 
     def post_process_prediction(self, prediction, post_processor=None):
         """ if post_processor is None we'll use all of them """
--- chord-grid/detection/downbeats.py
+++ chord-grid/detection/downbeats.py
@@ -1,13 +1,7 @@
-from madmom.features.downbeats import (RNNDownBeatProcessor, RNNBarProcessor,
-    DBNDownBeatTrackingProcessor, DBNBarTrackingProcessor, SyncronizeFeaturesProcessor)
-
-# RNNDownBeat
-# Takes audio Returns
-# Returns Downbeat probabilities
-
-# RNNBar
-# Takes Beat POSITIONS + audio
-# Returns Downbeat probabilities
+from madmom.features.downbeats import (
+    RNNDownBeatProcessor, RNNBarProcessor, DBNDownBeatTrackingProcessor,
+    DBNBarTrackingProcessor, SyncronizeFeaturesProcessor
+)
 
 # DBNDownBeatTracking
 # Takes Activation function with probabilities corresponding to beats and downbeats given in the first and second column, respectively. 
@@ -19,5 +13,61 @@ from madmom.features.downbeats import (RNNDownBeatProcessor, RNNBarProcessor,
 
 class DownBeats:
 
-    def __init__(self):
+    def __init__(self, beat_subdivisions=(4,2), beats_per_bar=[4],
+                 fps=100, min_bpm=55.0, max_bpm=215.0):
+        ''' beat_subdivions - tuple with # of beat subdivisions
+            for the percussive and harmonic feature. '''
+
+        # number of subdivisions a beat is divided into
+        self.beat_subdivisions = beat_subdivisions
+        self.beats_per_bar = beats_per_bar
+        self.fps = fps
+        self.min_bpm = min_bpm
+        self.max_bpm = max_bpm
+
+        self.downbeat_predictor = RNNDownBeatProcessor()
+        # downbeat_bar_predictor takes beat predictions as well as audio
+        self.downbeat_bar_predictor = RNNBarProcessor(
+            beat_subdivisions=self.beat_subdivisions, fps=self.fps
+        )
+
+        # maybe try experimenting with
+        # num_tempi=60, transition_lambda=100, observation_lambda=16,
+        self.downbeat_processor = DBNDownBeatTrackingProcessor(
+            beats_per_bar=self.beats_per_bar, fps=self.fps,
+            min_bpm = self.min_bpm, max_bpm=self.max_bpm
+        )
+
+        # maybe try experimenting with 
+        # observation_weight=100, meter_change_prob=1e-07
+        self.downbeat_bar_processor = DBNBarTrackingProcessor(
+            beats_per_bar=self.beats_per_bar
+        )
+
+        self.processors = [self.downbeat_processor, self.downbeat_bar_processor]
         return
+
+
+    def predict_downbeats(self, beats=None): 
+
+        if beats:
+            return
+        
+        return
+
+
+    def process_downbeats(self, prediction, processor=None):
+        """ if post_processor is None we'll use both of them """
+        downbeat_times = []
+        if not processor:
+            for idx, p in enumerate(self.processors):
+                p_beat_times = p(prediction) 
+                downbeat_times[idx] = p_beat_times
+            return downbeat_times
+
+        if processor not in self.processors:
+            raise ValueError(f'Invalid processor {processor}'
+                             f'valid post processors are {self.processors}.')
+
+        beat_times = processor(prediction)
+        return beat_times
