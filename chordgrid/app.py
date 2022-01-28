b9f3c8389d578c06a9d88d4a8d0c2cf8a7c7838a
morgan <mwcmitchell@gmail.com>
2022-01-28 12:58:40 -0800

    started audacity tools

--- chordgrid/app.py
+++ chordgrid/app.py
@@ -3,9 +3,9 @@ from chordgrid.detection.beats import Beats
 from chordgrid.detection.downbeats import DownBeats
 
 def run():
-    print('starting inits')
+    print('init')
     beats = Beats()
     downbeats = DownBeats()
     chords = Chords()
-    print('done inits')
+    print('done init')
     return
--- chordgrid/tools/audacity.py
+++ chordgrid/tools/audacity.py
@@ -1,23 +1,27 @@
+import sys
+import os
 
 class Audacity:
 
-    def __init__(self, audio_file_path=None, audacity_path=None, pipe_names=None):
+    def __init__(self, audio_file_path=None, audacity_path=None):
         self.audio_file_path = audio_file_path
         self.audacity_path = audacity_path
+        self.TONAME = '/tmp/audacity_script_pipe.to.' + str(os.getuid())
+        self.FROMNAME = '/tmp/audacity_script_pipe.from.' + str(os.getuid())
+        self.EOL = '\n'
 
-            #if sys.platform == 'win32':
-                #print("pipe-test.py, running on windows")
-                #TONAME = '\\\\.\\pipe\\ToSrvPipe'
-                #FROMNAME = '\\\\.\\pipe\\FromSrvPipe'
-                #EOL = '\r\n\0'
-            #else:
-                #print("pipe-test.py, running on linux or mac")
-                #TONAME = '/tmp/audacity_script_pipe.to.' + str(os.getuid())
-                #FROMNAME = '/tmp/audacity_script_pipe.from.' + str(os.getuid())
-                #EOL = '\n'
+        if sys.platform == 'win32':
+            self.TONAME = '\\\\.\\pipe\\ToSrvPipe'
+            self.FROMNAME = '\\\\.\\pipe\\FromSrvPipe'
+            self.selfEOL = '\r\n\0'
 
-        self.open_audacity(self.audacity_path)
+        if not os.path.exists(self.TONAME):        
+            raise Exception(f'No file {self.TONAME}, ensure audacity is running with mod-script-pipe')
+        if not os.path.exists(self.FROMNAME):        
+            raise Exception(f'No file {self.FROMNAME}, ensure audacity is running with mod-script-pipe')
 
+        self.TOFILE = open(self.TONAME, 'w')
+        self.FROMFILE = open(self.FROMNAME, 'rt')
         return
 
     
@@ -26,29 +30,43 @@ class Audacity:
         return
 
 
-    def connect(self):
-        ''' connect via mod-script-pipe '''
-        return
-
-
     def import_audio(self, audio_file_path=None):
         # try self.audio_file_path if none provided
         return
 
         
-    # note: this might be overkill
-    # def do_command(self):
-        # this need command, options args
-        # return
+    def send_command(self, command):
+        self.TOFILE.write(command + self.EOL)
+        self.TOFILE.flush()
+        return
+
+    def get_response(self):
+        result = ''
+        line = ''
+        while True:
+            result += line
+            line = self.FROMFILE.readline()
+            if line == '\n' and len(result) > 0:
+                break
+        return result
+
+
+    def do_command(self, command):
+        self.send_command(command)
+        response = self.get_response()
+        return response
 
 
     def add_label_track(self, track_name):
+        self.do_command('')
         return
 
 
-    def add_label(self, label_ts, label_txt):
+    def add_label(self, ts, txt):
+        self.do_command('AddLabel: ')
+        self.do_command(f'SetLabel: Label="0" Start="{ts}" End="{ts}" Text="{txt}"')
         return
 
 
-    def save_project(self, project_name):
+    def save_project(self, path):
         return
